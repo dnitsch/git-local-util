531d63953e111121c57ffdc0521371773331739a
dnitsch <nitschneiderd@gmail.com>
2023-05-02 11:12:36 +0100

    fix: add windows checker

--- internal/migrate/migrate.go
+++ internal/migrate/migrate.go
@@ -3,6 +3,7 @@ package migrate
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	log "github.com/dnitsch/simplelog"
@@ -10,9 +11,18 @@ import (
 )
 
 const (
-	INI_SECTION     = `remote "origin"`
-	INI_PROPERTY    = "url"
-	GIT_CONFIG_FILE = ".git/config"
+	INI_SECTION  = `remote "origin"`
+	INI_PROPERTY = "url"
+)
+
+var (
+	get_config_file = func() string {
+		if runtime.GOOS == "windows" {
+			return ".git\\config"
+		}
+		return ".git/config"
+	}
+	GIT_CONFIG_FILE = ""
 )
 
 type Migrate struct {
@@ -47,34 +57,37 @@ func (o Origin) Replace(find, replace string) string {
 // loads the INI config file and attemps to perform a replacement
 func (m Migrate) ReplaceConfigOrigin(parentDir string) error {
 	m.log.Debugf("attempting to read parentDir: %s", parentDir)
-	return filepath.WalkDir(parentDir, m.walkFunc)
+	gcf := get_config_file()
+	m.log.Debugf("using gitconfig: %s", gcf)
+	return filepath.WalkDir(parentDir, m.walkFunc(gcf))
 }
 
-func (m Migrate) walkFunc(path string, info os.DirEntry, err error) error {
-	if err != nil {
-		m.log.Errorf("error reading dir in walkDirFunc: %v", err)
-		return err
-	}
-
-	if !info.IsDir() && strings.Contains(path, GIT_CONFIG_FILE) {
-		// fmt.Println(path, info.Size())
-		m.log.Debugf("found git config path: %s\n", path)
-		cfg, err := ini.Load(path)
+func (m Migrate) walkFunc(gitconfig string) func(path string, info os.DirEntry, err error) error {
+	return func(path string, info os.DirEntry, err error) error {
 		if err != nil {
-			m.log.Errorf("error loading ini file from path: %v", err)
+			m.log.Errorf("error reading dir in walkDirFunc: %v", err)
 			return err
 		}
-		gm := GitConfigMap{iniFile: cfg, File: path}
+		if !info.IsDir() && strings.Contains(path, gitconfig) {
+			m.log.Debugf("found git config path: %s\n", path)
+			cfg, err := ini.Load(path)
+			if err != nil {
+				m.log.Errorf("error loading ini file from path: %v", err)
+				return err
+			}
+			gm := GitConfigMap{iniFile: cfg, File: path}
 
-		if err := gm.setNewConfig(path, m.find, m.replace); err != nil {
-			m.log.Debugf("falied to set new value in git config path: %s\n", path)
-			return err
+			if err := gm.setNewConfig(path, m.find, m.replace); err != nil {
+				m.log.Debugf("falied to set new value in git config path: %s\n", path)
+				return err
+			}
+			m.log.Debugf("successfully replaced origin in: %s, exchanged '%s' for '%s'", path, m.find, m.replace)
+
+			return nil
 		}
-		m.log.Debugf("successfully replaced origin in: %s, exchanged '%s' for '%s'", path, m.find, m.replace)
-
 		return nil
 	}
-	return nil
+
 }
 
 func (g GitConfigMap) setNewConfig(path, find, replace string) error {
